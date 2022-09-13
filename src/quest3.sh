771895cfeff4e95087a62113bcdf99b2d2bf011e
Meadows Ellis <meadowse@si-o1.kzn.21-school.ru>
2022-09-13 08:54:18 +0300

    quest1to6

---
+++ src/quest3.sh
@@ -0,0 +1,8 @@
+mkdir door_managment_files
+cd door_managment_files
+mkdir door_configuration
+mkdir door_map
+mkdir door_logs
+cp ../door_managment_fi/*.conf door_configuration
+cp ../door_managment_fi/*.log door_logs
+cp ../door_managment_fi/*.1 door_map
---
+++ src/quest6.sh
@@ -0,0 +1,6 @@
+cd ai_help
+sh keygen.sh
+cd key
+rm f*
+cd ..
+sh unifier.sh
